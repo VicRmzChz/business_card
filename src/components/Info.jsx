0e5570f5fb91b33c252d95f051369734ece4861b
VicRmzChz <vicentermzchz@gmail.com>
2022-04-28 01:45:01 -0500

    Add some info

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -5,6 +5,11 @@ export function Info() {
     return (
         <div className="info_section">
             <img src={require("../images/Face.png")} alt="face of cat" className="info--img" />
+            <div className="info--titles">
+                <h1>Blue</h1>
+                <h2>Frontend Developer WannaBe</h2>
+                <a href="#">Frontend WannaBe.com</a>
+            </div>
         </div>
     );
 }
