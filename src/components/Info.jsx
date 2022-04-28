3e8292c530078def31c437396e56da2c8cdbc772
VicRmzChz <vicentermzchz@gmail.com>
2022-04-27 23:03:24 -0500

    Making Info section

---
+++ src/components/Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export function Info() {
+    return (
+        <div className="info_section">
+            <img src={require("../images/Face.png")} alt="face of cat" className="info--img" />
+        </div>
+    );
+}
