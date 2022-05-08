6cca5fbd5be4e1aa7a7d725477a69b1f391ad977
VicRmzChz <vicentermzchz@gmail.com>
2022-05-08 13:21:32 -0500

    About and Footer add it and syle

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function Footer() {
+    return (
+        <div className="footer">
+            <div className="media_icons">
+                <a href="/"><i className="fa fa-github-alt"></i></a>
+                <a href="/"><i className="fa fa-fort-awesome"></i></a>
+                <a href="/"><i className="fa fa-odnoklassniki"></i></a>
+                <a href="/"><i className="fa fa-linux"></i></a>
+            </div>
+        </div>
+    );
+}
